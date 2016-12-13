5e49442c6ddc80b52382563fa850a5c5f1906f19
zsy <siyi.zheng@aalto.fi>
2016-12-13 12:42:36 +0200

    fix bugs

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, News=News, Player=Player,
                 Team=Team, Comment=Comment)
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
